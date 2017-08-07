f11c86ccd3ac73664c7aa474d88d6e5b6e29715f
trung <trung@mdkt.org>
2017-08-07 13:32:42 -0400

    check if repo has been cloned or not

--- list_repos.sh
+++ list_repos.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-curl -s https://api.github.com/users/terraform-community-modules/repos | grep "full_name" | awk -F'"' '{print $4}'
+for r in $(curl -s https://api.github.com/users/terraform-community-modules/repos | grep "full_name" | awk -F'"' '{print $4}')
+do
+   echo -n "$r --> "
+   rname=$(echo "$r" | awk -F'/' '{print $2}')
+   if [ -d "$rname" ]; then
+      echo "found"
+   else
+      echo "NOT FOUND"
+   fi
+done
