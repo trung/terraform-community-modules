9cfbce27bbfc8ee72ee7130f8178fac559804670
trung <trung@mdkt.org>
2017-08-07 14:15:28 -0400

    handled pagination

--- list_repos.sh
+++ list_repos.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-
-for r in $(curl -s https://api.github.com/users/terraform-community-modules/repos | grep "full_name" | awk -F'"' '{print $4}')
+has_repos=1
+page=1
+while [ "$has_repos" -eq "1" ];
 do
-   echo -n "$r --> "
-   rname=$(echo "$r" | awk -F'/' '{print $2}')
-   if [ -d "$rname" ]; then
+  has_repos=0
+  for r in $(curl -s "https://api.github.com/users/terraform-community-modules/repos?page=$page" | grep "full_name" | awk -F'"' '{print $4}')
+  do
+    has_repos=1
+    echo -n "$r --> "
+    rname=$(echo "$r" | awk -F'/' '{print $2}')
+    if [ -d "$rname" ]; then
       echo "found"
-   else
+    else
       echo "NOT FOUND"
-   fi
+      git clone git@github.com:$r.git
+    fi
+  done
+  page=$((page+1))
 done
